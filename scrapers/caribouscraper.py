1293396c6bba65bf9eb0cbcbb8234b11e16b7c2d
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-12-08 10:57:23 -0500

    Updated to create cleaner output, more columns

--- scrapers/caribouscraper.py
+++ scrapers/caribouscraper.py
@@ -60,16 +60,14 @@ zips.columns = ['zip code']
 
 ##############################################################
 
-
 # conversion into data frame object
 # now to go through the stores and extract the useful info
 
 zipsoup = [soupify(i)for i in zips['zip code']]
-# first runthrough gets the point coordinates, store name, street, town
+# first runthrough gets the point coordinates
 storeframe = [storeFrame(i)for i in zipsoup]
-# second runthrough gets the zip code
-storezips = [zipFrame(i) for i in zipsoup]
-
+# second runthrough gets the zip code and other info
+storeaddress = [addressFrame(i) for i in zipsoup]
 
 datadir = '~/data/'
 
@@ -77,23 +75,42 @@ biglist = pd.DataFrame()
 for i in range(0,len(storeframe)):
     biglist = biglist.append(storeframe[i])
 biglist.index = range(len(biglist))
-# making zip code list
-zip_code = []
-for i in range(0,len(storezips)):
-    for j in range(0, len(storezips[i])):
-            zipper = unicode(storezips[i][j].string)
-            zip_code.append(zipper)
-zip_code = pd.DataFrame(zip_code)
-zip_code.columns = ['zip code']
+biglist = biglist.rename(columns = {'altTagText': 'address'})
+
+bigstore = pd.DataFrame()
+for i in range(0,len(storeaddress)):
+    bigstore = bigstore.append(storeaddress[i])
+bigstore.index = range(len(bigstore))
 
 # merging stores and zips 
-biglist = biglist.join(zip_code)
+biglist = biglist.join(bigstore)
 # removing duplicate locations - there will be a lot!
 biglist = biglist.drop_duplicates('id')
 # cleaning data - zip code 55111 doesn't have a shapefile
 # it is part of the MSP airport
 # so we substitute zip code 55450 whose shapefile covers the airport
 biglist = biglist.replace(u'55111', u'55450')
+
+# making sure only stores with a TC area zip are in the frame
+biglist = biglist.merge(zips, on='zip code', how='inner')
+
+# renumber index column
+biglist.index = range(len(biglist))
+
+# parsing feature list
+for i in range(0, len(biglist['features'])):
+   for j in range(0, len(biglist['features'][i])):
+       namely = biglist['features'][i][j]
+       print('store ' + str(i))
+       if namely in biglist:
+           biglist[namely][i] = 1
+       else:
+           biglist[namely]=0
+           biglist[namely][i] = 1
+
+biglist = biglist.drop(['type', 'get_directions_url', 'url', 'features', 'None'], axis=1)
+for i in range(0, len(biglist['address'])):
+    biglist['address'][i] = biglist['address'][i].replace(u'Location at ', u'')
 biglist.to_csv(datadir+ "twincitycaribou.csv", index = False)
 
 # creating list of store counts by zip code
