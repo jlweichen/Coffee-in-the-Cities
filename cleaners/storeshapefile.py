5603b2dff7a7a184f58531b06b7cf1df7cede49d
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-12-29 20:08:58 -0500

    Update storeshapefile.py

--- cleaners/storeshapefile.py
+++ cleaners/storeshapefile.py
@@ -5,15 +5,14 @@ import pandas as pd
 star = pd.read_csv('~/Documents/cariboucity/scrapers/data/twincitysbux.csv')
 bou = pd.read_csv('~/Documents/cariboucity/scrapers/data/twincitycaribou.csv')
 
-
-# I want to join the data from both stores into one frame that has store name (Caribou or Starbucks), store id, and coordinates
-# this is the data I will eventually use for ML, but also for Tableau and visualization.
+# I want to join the data from both stores into one frame that has store name (Caribou or Starbucks),
+# store id, and coordinates.
+# This is the data I will eventually use for ML, but also for Tableau and visualization.
 # Becauze ZCTA and USPS zip code do not 100% match, and website has potential for misentered data,
 # I will use GIS to create a new zip code column based on polygon where lat and long point lies
 
 # note I want to work in projection NA83 / UTM zone 15N
 # aka EPSG: 26915
-# easier to transform a few hundred points than a hundred multipolygons!
 
 # first, I'm making sure each row states whether it is Caribou or Starbucks by creating a 'brand' column
 bou['brand'] = 'Caribou'
@@ -24,17 +23,16 @@ del(bou, star)
 # keeping the brand, latitude, longitude, id, and city information
 both = both[['brand', 'latitude', 'longitude', 'id', 'city']]
 both.index = (range(len(both)))
-# exporting to csv archive
-both.to_csv('~/Documents/cariboucity/scrapers/data/cleanstores.csv')
 
 # now to begin the GIS work of finding each store's zip code / ZCTA
-# must make the data from the 'both' frame into a dictionary for use w Shapely and Fiona
+# must make the data from the 'both' frame into a dictionary for use w Shapely and Fiona,
+# which work better with something resembling GeoJSON
 storepoints = both.to_dict(orient = 'records')
-# adding point info to each store in storepoints
+# adding point geometry to each store in storepoints requires the Shapely library
 from shapely.geometry import Point
 # pyporj library handles projections
 import pyproj
-# oldProj is the projection of the Google web service
+# oldProj is the projection of the Google web service - how our scraped data was projected
 oldProj = pyproj.Proj(init='epsg:3857')
 # newProj is the GIS projection of the Census shapefiles
 newProj = pyproj.Proj(init='epsg:26915')
@@ -47,6 +45,7 @@ for row in storepoints:
     row['point'] = Point(x1, y1)
 # now to read in the shapefile of ZCTAs created with 'countyfips.py'
 # we want to add zip code info for each store based in what zip code polygon it's coords fall into
+# https://automating-gis-processes.github.io/2016/Lesson3-point-in-polygon.html#how-to-check-if-point-is-inside-a-polygon
 
 # first reading zip code shapefile with fiona
 import fiona
@@ -62,13 +61,10 @@ for i in(range(len(zipframe))):
 # going through each point and seeing where it falls within a polygon
 # id 34 and 42 and 55 are a single polygon each
      # point 78 should fall in polygon 55
-# id 3 is a multipolygon
+
      
 There are 410 stores and only 185 shapefiles/zip codes
-Each store has a zip, but not every zip has a store
-only getting zip code data for Metro Council counties
-however, I am leaving in all stores in case I want to calculate
-average distance, stores in x mile radius, etc.
+will iterate though stores, stop if/when a zip is found
 '''
 for i in range(len(storepoints)):
     storepoints[i]['zip code'] = 'NaN'
@@ -100,3 +96,8 @@ with collection(
                     'city': row['city']
                 }
             })
+    
+# exporting store data to csv archive - will later join this data with ACS data
+storepoints = pd.DataFrame.from_dict(storepoints)
+storepoints = storepoints.drop(labels = 'point', axis=1)
+storepoints.to_csv('~/Documents/cariboucity/scrapers/data/cleanstores.csv')
