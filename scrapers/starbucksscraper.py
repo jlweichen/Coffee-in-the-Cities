379f858b78bbca0f478de4b434e2fb2aa1019239
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-12-30 21:06:43 -0500

    Update starbucksscraper.py

--- scrapers/starbucksscraper.py
+++ scrapers/starbucksscraper.py
@@ -10,7 +10,7 @@ import json
 
 import pandas as pd
 ##################################################
-# auhtentication of Google Maps AI
+# authentication of Google Maps AI
 gmaps = googlemaps.Client(key='')
 
 ##################################################
