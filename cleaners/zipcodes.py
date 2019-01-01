07f81f63dfaa64208f2124a834406c66ffd9b353
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-12-31 23:19:30 -0500

    Fixed some comment typos

--- cleaners/zipcodes.py
+++ cleaners/zipcodes.py
@@ -1,6 +1,6 @@
-# The purpose of this file is to take a shapefile of ZCTAs for Minnesota
+# The purpose of this file is to take a shapefile of ZCTAs from the Census
 # and extract the zip codes that lie (either wholly or partially) in a county
-# considered part of the Twin Cities metropolitan area by the Census
+# considered part of the Twin Cities metropolitan area
 
 import pandas as pd
 import geopandas as gpd
@@ -46,19 +46,18 @@ metrofips['ZCTA5'] = metrofips['ZCTA5'].astype('unicode')
 # taking the zips of interest from a locally saved copy of shapefile:
 # ZIP Code Tabulation Areas 2010 Census
 # this is using the file directly provided by the Census - it is in GCS NAD83 projection
-# fro the .prj file directly, it is:
+# from the .prj file directly, it is:
 # GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",
 # SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],
 # UNIT["Degree",0.017453292519943295]]
 # aka EPSG 4269
 zipshape = gpd.read_file('/Users/jennifer/Documents/GIS/GIS data/TIGER/tl_2018_us_zcta510/tl_2018_us_zcta510.shp')
-
 zipshape = zipshape.rename(columns = {'ZCTA5CE10': 'ZCTA5'})
 somezips = zipshape.merge(metrofips, on = 'ZCTA5', how = 'right')
-# projection of outgoing shapefile
-# want to change from EPSG 4269 to EPSG 26915
-# running somezips.crs in the console will show EPSG 4269 is the current projection
 
+# Now to change the projection of outgoing shapefile -
+# I want to change it from EPSG 4269 to EPSG 26915
+# running somezips.crs in the console will show EPSG 4269 is the current projection.
 # Geopandas frames have a function .to_crs, which I will use to reproject the
 # shapefile before I write it to disk
 somezips = somezips.to_crs({'init': 'epsg:26915'})
