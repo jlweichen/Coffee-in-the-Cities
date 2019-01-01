ba91f2a0aa12a6d1a7ec7ff0cc167f8df47b6cc4
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-12-31 23:13:37 -0500

    Update censusblocks.py

--- cleaners/censusblocks.py
+++ cleaners/censusblocks.py
@@ -35,15 +35,23 @@ wicounties = pd.DataFrame({'County Name': ['Pierce', 'Saint Croix'],
 wifips = wisconsin.merge(wicounties, on='COUNTYFP10', how='inner')
 
 # put them together - now we have all the Census blocks in the Twin Cities MSA
-both = mnfips.append(wifips)
+both = mnfips.append(wifips)             
+del(minnesota, wisconsin, mncounties, wicounties, mnfips, wifips)
+
+# making sure index is nice and clean
 both.index = range(0, len(both))
-# add a column that has block group - will need later for ACS data
+# adding a column that has block group - will need later for ACS data
 both['BLOCK GROUP'] = [i[0:12] for i in both['GEOID10']]
 # removing columns that aren't of interest
 both = both.drop(['STATEFP10', 'COUNTYFP10', 'TRACTCE10', 'BLOCKCE10', 'NAME10', 
-                  'MTFCC10', 'FUNCSTAT10', 'ALAND10', 'AWATER10', 'INTPTLAT10', 'INTPTLON10'], axis=1)
-                
-del(minnesota, wisconsin, mncounties, wicounties, mnfips, wifips)
+                  'MTFCC10', 'FUNCSTAT10', 'ALAND10', 'AWATER10', 'INTPTLAT10', 
+                  'INTPTLON10', 'UACE10', 'UATYPE'], axis=1)
+# the column UR10 is a Census flag - is this block urban or rural?
+# we can rename and clean this column - it may be valuable info
+# let's call it Urban - value is 1 if the block is urban and 0 if it isn't
+both = both.rename(columns = {'UR10': 'Urban'})
+# if both['Urban'] is U, reassign it 1, or 0 if it is R
+both['Urban'] = both['Urban'].apply(lambda x: 1 if x=='U' else 0)
 
 # projection of outgoing shapefile
 # want to change from EPSG 4269 to EPSG 26915
@@ -55,22 +63,21 @@ both.to_file(datadir+ 'edited/tczips/twincitiesblocks.shp')
 # reading in stores data, using the shapefile created with storeshapefile.py
 stores = gpd.read_file("/Users/jennifer/Documents/cariboucity/sourcegis/edited/tczips/stores2018-12-31/stores2018-12-31.shp")
 
-# creating storecount column in 'both' 
-#both['Coffee'] = 0
+# creating storecount column in 'both' called 'Coffee'
 # iterating through polygons of the Census blocks
 # will flag 0 if no stores, 1 if there is a store
-# very very few blocks have multiple stores less than 10 have three or more
+# very very few blocks have multiple stores - less than 10 have three or more!
 # at the block level, better off doing some binary prediction like logistic regression
 
+# writing a function that uses the Shapely .contains() method for polygons
 def caffeine(x):
     runs = [x.contains(y) for y in stores['geometry']]
     if any(runs) == True:
         return 1
     else:
         return 0
-     
+# calling the function on the geometry column of the 'both' dataframe
 both['Coffee'] = both['geometry'].apply(caffeine)
-
 
 # writing to shapefile
 both.to_file(datadir+ 'edited/tczips/twincitiesblockswithstorecounts.shp')
@@ -78,31 +85,37 @@ both.to_file(datadir+ 'edited/tczips/twincitiesblockswithstorecounts.shp')
 # no longer need block geometry, will drop that
 both = both.drop(['geometry'], axis=1)
                 
-# ACS 5 year summary data by block group - a tract contains one or more block
+# ACS 5 year summary data broken down by block group - a tract contains one or more block
 # groups, each of which contain one or more blocks.
-# This file is compiled and published by Metropolitan Council, and includes
+# This file is compiled and published by the Metropolitan Council, and includes
 # counties outside of its 7-county jurisdiction.
 # Metadata can be found at:
 # ftp://ftp.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/society_census_acs/metadata/metadata.html
 
 acs = pd.read_excel('/Users/jennifer/Documents/cariboucity/sourcegis/raw/xlsx_society_census_acs20132017/CensusACSBlockGroup.xlsx',
-                    dtype = {'GEOG_UNIT':'unicode', 'TRACT':'unicode'})
-acs = acs.rename(columns={'GEOG_UNIT': 'BLOCK GROUP', 'STATE': 'STATEFP10', 'COUNTY': 'COUNTYFP10'})
-# dropping some less useful columns
-acs = acs.drop(['USBORNCIT', 'FORBORNCIT', 'FORBORNNOT', 'CDENOM', 'CDENOM_017',
+                    dtype = {'GEOG_UNIT':'unicode'})
+acs = acs.rename(columns={'GEOG_UNIT': 'BLOCK GROUP'})
+# dropping some columns, like those around citizenship, disability, language
+# choosing not to use these in analysis - I'd prefer to look at age, income, housing,
+# education, and race
+acs = acs.drop(['GEOG_LEVEL', 'GEOID', 'GEOID2', 'GEONAME', 'SUMLEV', 'COUNTY', 'GEOCOMP', 'YEAR',
+                'USBORNCIT', 'FORBORNCIT', 'FORBORNNOT', 'CDENOM', 'CDENOM_017',
                 'CDENOM_517', 'CDENOM1864', 'CDENOM65UP', 'ANYDIS', 
                 'ANYDIS_017', 'ANYDIS1864', 'ANYDIS65UP', 'DEAF', 'DEAF_017',
                 'DEAF1864', 'DEAF65UP', 'VISION', 'VISION_017', 'VISION1864', 'VISION65UP',
                 'COGDIS', 'COGDIS_517', 'COGDIS1864', 'COGDIS65UP', 'AMBDIS', 'AMBDIS_517',
                 'AMBDIS1864', 'AMBDIS65UP', 'SELFCARE' , 'SELFCA_517', 'SELFCA1864',
-                'SELFCA65UP', 'INDLIV'], axis=1)
+                'SELFCA65UP', 'INDLIV', 'INDLIV_517', 'INDLIV1864', 'INDLIV65UP',
+                'ENGLISH', 'ESL_VWELL', 'LEP', 'LEP_SPAN', 'LEP_RUSS', 'LEP_CHIN',
+                'LEP_HMONG', 'LEP_VIET', 'LEP_AFRICA'], axis=1)
 
 both=both.merge(acs, on='BLOCK GROUP', how = 'inner')
 
 # Let's bring in the Census LODES data.
 # Workplace Area Characteristics would probably be useful - don't people drink coffee at work?
 # Our data is from 2015, and covers all jobs. Data from other years and job segments
-# is also available online
+# are also available online.
+# I'm using the WAC datasets from 2015 for Minnesota and Wisconsin
 
 MNlodes = pd.read_csv('/Users/jennifer/Documents/GIS/GIS data/LODES/mn_wac_S000_JT00_2015.csv')
 WIlodes = pd.read_csv('/Users/jennifer/Documents/GIS/GIS data/LODES/wi_wac_S000_JT00_2015.csv')
@@ -111,21 +124,22 @@ bothlodes.index = range(0, len(bothlodes))
 # see https://lehd.ces.census.gov/data/lodes/LODES7/LODESTechDoc7.3.pdf
 # provides summary of the data table - each row is a Census block
 
-# some columns contain data that probably won't be relevant
+# I am choosing to drop some columns, mostly ones regarding worker race and the
+# age of the employing firm
 bothlodes= bothlodes.drop(['CFA01', 'CFA02', 'CFA03', 'CFA04', 'CFA05', 'CFS01', 
-                           'CFS02', 'CFS03', 'CFS04', 'CFS05', 'createdate'], axis=1)
+                           'CFS02', 'CFS03', 'CFS04', 'CFS05', 'CR01', 'CR02',
+                           'CR03', 'CR04', 'CR05', 'CR07', 'CT01', 'CT02', 'createdate'], axis=1)
+# renaming some other columns so they are less confusing, and can be joined to other data
+bothlodes = bothlodes.rename(columns={'w_geocode': 'GEOID10', 'C000': 'Total jobs'})
+
 # GEOID10 column of the block shapefile needs to be joined with the w_geocode
 # column of the LODES flat files - rename and make sure it's of string type so it can be joined
-bothlodes = bothlodes.rename(columns = {'w_geocode': 'GEOID10'})
 bothlodes['GEOID10'] = bothlodes['GEOID10'].astype(str)
 
 both = both.merge(bothlodes, on='GEOID10', how='left')
 # replacing NA values with zero, for blocks without any jobs
 both = both.fillna(0)
-
-
+# renaming GEOID10 to Block Name
+both = both.rename(columns = {'GEOID10': 'Block Name'})
 # writing machine learning data to csv
-mlfile = both
-
-mlfile = mlfile.drop(['TRACTCE10', 'BLOCKCE10'], axis=1)
-mlfile.to_csv('/Users/jennifer/Documents/cariboucity/ml.csv')
+both.to_csv('/Users/jennifer/Documents/cariboucity/ml.csv', index = False)
